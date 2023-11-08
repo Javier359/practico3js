6851c7b2279a2c58571e589cd968990df92c6527
Javier Jimenez <javier.sniguel@gmail.com>
2023-11-08 00:03:45 -0300

    Se realizo el ejercicio 5 del tp de funciones

---
+++ js/ej5.js
@@ -0,0 +1,15 @@
+const texto = prompt("Ingresar el texto porfavor");
+
+function analizarCadena(cadena) {
+    if (cadena === cadena.toUpperCase()) {
+      return alert( "La cadena está formada solo por mayúsculas.");
+    }
+    else if (cadena === cadena.toLowerCase()) {
+      return alert( "La cadena está formada solo por minúsculas.");
+    }
+    else {
+      return alert( "La cadena es una mezcla de mayúsculas y minúsculas.");
+    }
+  }
+
+  analizarCadena(texto);
