de6f2e3d3cda944fa0eef0d9a7c78a8d5bf1f807
Javier Jimenez <javier.sniguel@gmail.com>
2023-11-07 22:55:08 -0300

    Se realizo las condiciones del ejercicio 2 y se lo finalizo

--- js/ej2.js
+++ js/ej2.js
@@ -1,5 +1,14 @@
 const ciudades = [];
-
+/*recorrido de vector */
+function mostrarVector (){
+    document.write(`<h2>Arreglo de ciudades</h2>`);
+    document.write(`<ul>`);
+    for(let i=0; i<ciudades.length; i++){
+        document.write(`<li>${ciudades[i]}</li>`);
+    }
+    document.write(`</ul>`);
+}
+/*Menu */
 while(true){
     const ciudad = prompt("Ingrese el nombre de una ciudad porfavor");
 
@@ -11,5 +20,18 @@ while(true){
         ciudades.push(ciudad);
     }
 }
-console.log(ciudades);
+/*condiciones */
+document.write(`<p>El arreglo de ciudades tiene ${ciudades.length} elementos</p>`);
+document.write(`<ul>`);
+document.write(`<li> <p>El elemento de la primera posicion es : ${ciudades[0]}</p> </li>`);
+document.write(`<li> <p>El elemento de la tercera posicion es : ${ciudades[2]}</p> </li>`);
+document.write(`<li> <p>El elemento de la ultima posicion es : ${ciudades[ciudades.length-1]}</p> </li>`);
+ciudades.push("Paris");
+document.write(`<li> <p>El elemento de la ultima posicion agregado es : ${ciudades[ciudades.length-1]}</p> </li>`);
+document.write(`<li> <p>El elemento de la segunda posicion es : ${ciudades[1]}</p> </li>`);
+ciudades[1]="Barcelona"
+
+mostrarVector();
+
+
     
